1a75b7b08213cafbd95ffb2aeeb650097dd11d33
Sarath <sarathiscookie@hotmail.com>
2017-05-14 21:37:56 +0530

    Auth files

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test', 'TestController@index')->name('test');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
